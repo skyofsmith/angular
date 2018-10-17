fe416329ba5aacfde55bc5524a744e3b536e2660
drzz <skyofsmith@163.com>
2018-10-17 22:48:39 +0800

    move angular practice code from myPractice

---
+++ ng2Book/demo5/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forms-demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class FormsDemoApp {
+  title = 'app';
+}
