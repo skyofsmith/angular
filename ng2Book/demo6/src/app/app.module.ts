de75c0f1f55a243b9c3048bd74e0691939c0c84d
drzz <skyofsmith@163.com>
2019-03-11 22:58:07 +0800

    import http module.

--- ng2Book/demo6/src/app/app.module.ts
+++ ng2Book/demo6/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component } from '@angular/core'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { HttpModule } from '@angular/http'
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule // <--- right here
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: []
 })
-export class AppModule { }
+class HttpAppModule {}
